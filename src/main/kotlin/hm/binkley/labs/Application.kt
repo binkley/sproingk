c3ecfdb3214ac10529013dda564e3924f4998122
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2016-11-28 07:57:51 -0600

    Hello world in REST, with Sleuth

--- src/main/kotlin/hm/binkley/labs/Application.kt
+++ src/main/kotlin/hm/binkley/labs/Application.kt
@@ -1,12 +1,20 @@
 package hm.binkley.labs
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.context.annotation.ComponentScan
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@EnableAutoConfiguration
-@ComponentScan
-class Application
+@SpringBootApplication
+@RestController
+open class Application {
+    @RequestMapping("/")
+    fun home(): String {
+        LoggerFactory.getLogger(Application::class.java).info("Hello, world!")
+        return "Hello, world!"
+    }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
