07deffceb121a87c884fe5d0dcb2f61d18101d3c
Brian Oxley <boxley@thoughtworks.com>
2016-12-08 08:21:45 -0600

    Closes #6

--- src/main/kotlin/hm/binkley/labs/Application.kt
+++ src/main/kotlin/hm/binkley/labs/Application.kt
@@ -4,12 +4,14 @@ import org.slf4j.LoggerFactory
 import org.springframework.boot.Banner.Mode.OFF
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod.GET
 import org.springframework.web.bind.annotation.RestController
 
 val logger = LoggerFactory.getLogger(Application::class.java)!!
 
+@EnableHystrixDashboard
 @SpringBootApplication
 @RestController
 open class Application {
