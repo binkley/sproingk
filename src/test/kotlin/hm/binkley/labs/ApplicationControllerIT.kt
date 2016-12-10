f2becd6e80a65bc82d824f3278cd7ce528500170
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2016-12-10 17:32:37 -0600

    Basic support for issue #3, integration tests
    More work to go, such as not being so darn noisy!

---
+++ src/test/kotlin/hm/binkley/labs/ApplicationControllerIT.kt
@@ -0,0 +1,24 @@
+package hm.binkley.labs
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(webEnvironment = RANDOM_PORT,
+        classes = arrayOf(Application::class))
+internal open class ApplicationControllerIT {
+    @Autowired lateinit var restTemplate: TestRestTemplate
+
+    @Test
+    fun shouldRespondCheerfully() {
+        val response = restTemplate.getForObject("/", String::class.java)
+
+        assertEquals("Hello, world!\n", response)
+    }
+}
