8e91770d6e7ff394da4b1955594d54df9c10466e
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2016-12-19 23:51:11 -0600

    Hygiene

--- src/main/kotlin/hm/binkley/labs/Application.kt
+++ src/main/kotlin/hm/binkley/labs/Application.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod.GET
 import org.springframework.web.bind.annotation.RestController
 
-val logger = getLogger(Application::class.java)!!
+private val logger = getLogger(Application::class.java)!!
 
 @EnableHystrixDashboard
 @SpringBootApplication
