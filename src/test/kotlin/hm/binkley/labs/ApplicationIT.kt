31847e351a33ca2dfcc3c1d79891042498058f14
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2016-12-21 09:39:48 -0600

    Hide uglies

--- src/test/kotlin/hm/binkley/labs/ApplicationIT.kt
+++ src/test/kotlin/hm/binkley/labs/ApplicationIT.kt
@@ -5,18 +5,14 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
-import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @DisplayName("GIVEN a running application on a random port")
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-internal open class ApplicationIT {
-    @Autowired lateinit private var restTemplate: TestRestTemplate
-
+internal open class ApplicationIT : RestITBase() {
     @DisplayName("WHEN root URL is called")
     @Nested
     inner class Root {
@@ -26,8 +22,5 @@ internal open class ApplicationIT {
         @Test
         fun shouldRespondCheerfully()
                 = assertEquals("Hello, world!\n", get(path))
-
-        private fun get(path: String)
-                = restTemplate.getForObject(path, String::class.java)
     }
 }
