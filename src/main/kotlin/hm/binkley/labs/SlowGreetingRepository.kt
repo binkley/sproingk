13ded99ba25594be3e713d350d5c977e2a982d37
Brian Oxley <boxley@thoughtworks.com>
2017-01-26 18:24:07 -0500

    Hygiene

--- src/main/kotlin/hm/binkley/labs/SlowGreetingRepository.kt
+++ src/main/kotlin/hm/binkley/labs/SlowGreetingRepository.kt
@@ -6,8 +6,7 @@ import java.util.concurrent.Future
 import java.util.concurrent.TimeUnit.SECONDS
 
 class SlowGreetingRepository : GreetingRepository {
-    private val cache
-            = ConcurrentHashMap<String, Future<String?>>()
+    private val cache = ConcurrentHashMap<String, Future<String?>>()
 
     override fun create(name: String) {
         cache[name] = supplyAsync {
@@ -17,14 +16,11 @@ class SlowGreetingRepository : GreetingRepository {
     }
 
     override fun get(name: String): String? {
-        val job = cache(name)
+        val job = cache[name] ?: throw IndexOutOfBoundsException(name)
         return if (job.isDone) job.get()!! else null
     }
 
     override fun delete(name: String) {
         cache.remove(name)?.cancel(true)
     }
-
-    private fun cache(name: String)
-            = cache[name] ?: throw IndexOutOfBoundsException(name)
 }
