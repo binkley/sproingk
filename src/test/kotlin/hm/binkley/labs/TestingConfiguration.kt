f9c589f7236d8ed3ca838cf9c3aa9562071425ca
Brian Oxley <boxley@thoughtworks.com>
2017-04-22 08:39:44 -0500

    Hygiene

--- src/test/kotlin/hm/binkley/labs/TestingConfiguration.kt
+++ src/test/kotlin/hm/binkley/labs/TestingConfiguration.kt
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 
 @Configuration
-open internal class TestingConfiguration {
+internal class TestingConfiguration {
     @Bean
     @Primary
     open fun greetingRepository() = TestingGreetingRepository()
