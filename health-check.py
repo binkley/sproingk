74cb4c8a33304c173e6970e037f96821d563d308
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2017-06-15 13:57:56 -0500

    Missed file

---
+++ health-check.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import urllib.request, json, sys
+
+url = "http://sproingk-binkley.boxfuse.io:8080/application/health"
+# url = "http://localhost:8080/application/health"
+with urllib.request.urlopen(url) as url:
+    data = json.loads(url.read().decode())
+    sys.exit(0 if "UP" == data["status"] else 1)
