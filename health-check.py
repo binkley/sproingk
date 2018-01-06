5cdb298f4b60a1db3ba24733212f036fc74a4f46
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2018-01-06 08:47:47 -0600

    Spring Boot M7 endpoint update
    Help for local command line

--- health-check.py
+++ health-check.py
@@ -2,8 +2,8 @@
 
 import urllib2, json, sys
 
-url = "http://sproingk-binkley.boxfuse.io:8080/application/health"
-# url = "http://localhost:8080/application/health"
+url = "http://sproingk-binkley.boxfuse.io:8080/actuator/health"
+# url = "http://localhost:8080/actuator/health"
 url = urllib2.urlopen(url)
 data = json.loads(url.read().decode())
 sys.exit(0 if "UP" == data["status"] else 1)
