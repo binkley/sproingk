de0eb2e007bae9c3a1575e6cd351219dd6c0c69b
Brian Oxley <boxley@thoughtworks.com>
2018-11-01 07:32:27 -0500

    Dependency updates

--- src/test/kotlin/hm/binkley/labs/TestingConfiguration.kt
+++ src/test/kotlin/hm/binkley/labs/TestingConfiguration.kt
@@ -8,5 +8,5 @@ import org.springframework.context.annotation.Primary
 internal class TestingConfiguration {
     @Bean
     @Primary
-    fun greetingService() = TestingGreetingBackgroundService()
+    fun greetingBackgroundService() = TestingGreetingBackgroundService()
 }
